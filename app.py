ed42620753cb695aed3bdf0d694a48b8abb31e25
Ndumiso Mnguni <mngunikhulekani@yahoo.com>
2022-03-31 22:07:47 +0200

    Remove debugging from the app.py file has a potential of being bug

--- app.py
+++ app.py
@@ -360,4 +360,4 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--port", type=int, default=8080)
     args = parser.parse_args()
-    app.run_server( port=args.port, debug=True)
+    app.run_server( port=args.port)
